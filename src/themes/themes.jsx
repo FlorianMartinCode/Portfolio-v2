8d520c1856dc6d830996eec46ae33752c8ded9c2
Flo700 <florian.martin700@gmail.com>
2023-09-04 23:22:36 +0200

    modif navbar

--- src/themes/themes.jsx
+++ src/themes/themes.jsx
@@ -1,20 +1,29 @@
-// ThemeContext.js
+// Importation des dépendances React nécessaires
 import React, { createContext, useContext, useState } from 'react';
 
+// Crée un contexte de thème
 const ThemeContext = createContext();
 
+// Hook personnalisé pour accéder au thème et à la fonction de bascule du thème
 export const useTheme = () => {
+  // Utilise useContext pour accéder au contexte de thème
   return useContext(ThemeContext);
 };
 
+// Composant fournissant le contexte de thème à l'ensemble de l'application
 export const ThemeProvider = ({ children }) => {
+  // État local pour stocker le thème actuel (clair ou sombre)
   const [darkTheme, setDarkTheme] = useState(false);
 
+  // Fonction pour basculer entre le thème clair et sombre
   const toggleTheme = () => {
+    // Inverse la valeur de darkTheme
     setDarkTheme(!darkTheme);
   };
 
+  // Rendu du composant
   return (
+    // Utilisation de ThemeContext.Provider pour fournir le contexte aux enfants
     <ThemeContext.Provider value={{ darkTheme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
