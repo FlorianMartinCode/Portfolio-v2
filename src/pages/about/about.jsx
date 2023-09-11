a9e800c6e334890e13544b0689f988801a02dd89
Flo700 <florian.martin700@gmail.com>
2023-09-12 00:21:59 +0200

    modif height, add cursor mixin btn

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import iconInfo from '../../assets/info.png'
 import Collapse from '../../components/main/collapse/collapse'
 import HtmlImg from '../../assets/icon-skills/html.png'
